16d7700ae2960bcf0f205461d0162af88abdddfa
Clement Pajean <pajean@ensta-paristech.fr>
2012-05-29 12:00:37 -0700

    Removed sparco's operators

--- operators/@classOp/ctranspose.m
+++
@@ -1,9 +0,0 @@
-function result = ctranspose(A)
-
-%   Copyright 2008, Ewout van den Berg and Michael P. Friedlander
-%   http://www.cs.ubc.ca/labs/scl/sparco
-%   $Id: ctranspose.m 1027 2008-06-24 23:42:28Z ewout78 $
-
-A.adjoint = xor(A.adjoint,1);
-result = A;
-
--- operators/@classOp/uplus.m
+++
@@ -1,8 +0,0 @@
-function A = uplus(A)
-% Intentially left empty.
-
-%   Copyright 2008, Ewout van den Berg and Michael P. Friedlander
-%   http://www.cs.ubc.ca/labs/scl/sparco
-%   $Id: uplus.m 1027 2008-06-24 23:42:28Z ewout78 $
-
-
