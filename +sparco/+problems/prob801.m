18a51c964af25b456f75a9cef7261ae6a5a33940
Clement Pajean <pajean@ensta-paristech.fr>
2012-06-18 14:50:02 -0700

    Fixed prob801

--- +sparco/+problems/prob801.m
+++ +sparco/+problems/prob801.m
@@ -12,7 +12,7 @@ import sparco.tools.*
 [opts,varg] = parseDefaultOpts(varargin);
 [parm,varg] = parseOptions(varg,{'noseed'},{'m','n'});
 m           = getOption(parm,'m', 1400); % Size of images
-n           = getOption(parm,'n', 18); % Size of training data
+n           = getOption(parm,'n', 18); % Size of training data (<25)
 info.name   = 'classuncorr';
 
 % Return problem name if requested
@@ -23,31 +23,32 @@ if ~parm.noseed, randn('state',0); rand('state',0); end
 
 % Set up the data
 idx = randperm(32256); idx = idx(1:m);
-Restriction = opRestriction(32256,idx);
-imgidx = randperm(25); idx = idx(1:n);
+imgidx = randperm(25); imgidx = imgidx(1:(n+1));
 solset = randi(8);
-signal = imread(sprintf('%sprob80x_%d_%d.pgm', opts.datapath, solset, imgidx(1)));
+signal = imread(sprintf('%sprob80x_%d_%02d.pgm', opts.datapath, solset, imgidx(1)));
 signal = reshape(signal,[],1);
-signal = Restriction*signal;
 
 % Set up the operators
-imgsets = zeros(m,8*n);
+imgsets = zeros(32256,8*n);
 for i=1:8
     for j=1:n
         img = imread(sprintf('%sprob80x_%d_%02d.pgm', opts.datapath, i, imgidx(j+1)));
         img = reshape(img,[],1);
-        img = Restriction*img;
         imgsets(:,(i-1)*n+j) = img;
     end
 end
 B = opMatrix(imgsets);
+M = opRestriction(32256,idx);
 
 % Set up the problem
 data.signal = signal;
 data.x0     = solset;
 data.B      = B;
-data.b      = signal;
+data.M      = M;
+data.b      = M * signal;
+data.r      = zeros(size(data.b));
 data        = completeOps(data);
+
 
 end
 
