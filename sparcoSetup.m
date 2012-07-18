4fc2270627e086136e9252969394edd480b07aeb
Clement Pajean <pajean@ensta-paristech.fr>
2012-07-18 12:27:32 -0700

    Changed sparcoSetup to check if Spot is already in the path

--- sparcoSetup.m
+++ sparcoSetup.m
@@ -7,8 +7,6 @@ function sparcoSetup(varargin)
 %   http://www.cs.ubc.ca/labs/scl/sparco
 %   $Id: sparcoSetup.m 1027 2008-06-24 23:42:28Z ewout78 $
 
-%   SPARCOSETUP('spotpath', PATH) will check for Spot's toolbox in PATH, if
-%   it is not found it will download and extract it in the default forlder.
 
 import sparco.tools.*
 
@@ -21,12 +19,6 @@ root = fileparts(which(mfilename));
 % Add Sparco subdirs to path.
 % ----------------------------------------------------------------------
 addtopath(root,'');
-%addtopath(root,'examples');
-%addtopath(root,'operators');
-%addtopath(root,'problems');
-%addtopath(root,'tools');
-%addtopath(root,['tools' filesep 'nufft']);
-%addtopath(root,['tools' filesep 'rwt']);
 
 % Make sure that the build subdir exists
 buildpath = {['build'],                          ...
@@ -45,115 +37,62 @@ for i=1:length(buildpath)
 end
 
 % ----------------------------------------------------------------------
-% Parse parameters.
-% ----------------------------------------------------------------------
-
-% % ----------------------------------------------------------------------
-% % Check for external dependencies.
-% % ----------------------------------------------------------------------
-% 
-% % Commented May 25th
-% 
-% % CurveLab: Req'd for 51-52
-% if exist('curvelab.pdf','file')
-%    crvroot = fileparts(which('curvelab.pdf'));
-%    addtopath(crvroot,'fdct_usfft_matlab');
-%    addtopath(crvroot,'fdct_wrapping_matlab');
-%    addtopath(crvroot,'fdct_wrapping_cpp/mex');
-%    addtopath(crvroot,'fdct3d/mex');
-% else
-%    fprintf(['\nWarning: CurveLab is not in the path. Problems 50-51 ' ...
-%             'will not work.\n\n']);
-% end
-% 
-% [T,result] = evalc('savepath;');
-% rehash path;
-% if result == 1
-%    fprintf(['\n' ...
-%             '\n Warning: SPARCO successfully added to your path,' ...
-%             '\n          but couldn''t make these changes permanent.' ...
-%             '\n          To permanently add SPARCO to your path,'...
-%             '\n          copy and paste the following lines into your'...
-%             '\n          startup.m file (e.g., ~/matlab/startup.m).'...
-%             '\n\n']);
-%    for i=1:length(pathlist)
-%      fprintf(' addpath(''%s'');\n',pathlist{i});
-%    end
-%    fprintf('\n');
-% end
-% 
-% % --'+sparco/',--------------------------------------------------------------------
-% % Compile the Rice Wavelet Toolbox
-% % ----------------------------------------------------------------------
-% 
-% if ~opts.norwt
-%    cd([root filesep 'tools' filesep 'rwt'])
-%    fprintf('Compiling the Rice Wavelet Toolbox MEX interfaces...');
-%    try
-%       if exist('mdwt'  ,'file')~=3, mex mdwt.c   mdwt_r.c;   end
-%       if exist('midwt' ,'file')~=3, mex midwt.c  midwt_r.c;  end
-%       if exist('mrdwt' ,'file')~=3, mex mrdwt.c  mrdwt_r.c;  end
-%       if exist('mirdwNew Foldert','file')~=3, mex mirdwt.c mirdwt_r.c; end
-%       fprintf('Success!\n');
-%   catch
-%       warning('Could not compile Rice Wavelet Toolbox MEX interfaces.');
-%   end
-%   cd(root)
-% end
-
-% ----------------------------------------------------------------------
 % Check if Spot toolbox is installed and install it if not 
 % ----------------------------------------------------------------------
+try spot.path;
+    fprintf('Spot toolbox found and in your path\n');
+catch err
+    fprintf('Spot toolbox not in the path\n');
+    if(~exist(fullfile(root,'spotbox'),'dir'))
+        fprintf('Spot toolbox not found\n');
+        fprintf('   Downloading Spot...\n');
+        spotwebsite = 'http://www.cs.ubc.ca/labs/scl/spot/';
+        website = urlread(spotwebsite);
+        dl = strfind(website, 'Download');
+        website = website(dl(1):size(website,2));
+        spotURL = strfind(website, '<a href="');
+        website = website((spotURL(1)+9):size(website,2));
+        endofURL = strfind(website,'"');
+        website = website(1:(endofURL-1));
 
-if(~exist(fullfile(root,'spotbox'),'dir'))
-    fprintf('Spot toolbox not found\n');
-    fprintf('   Downloading Spot...\n');
-    spotwebsite = 'http://www.cs.ubc.ca/labs/scl/spot/';
-    website = urlread(spotwebsite);
-    dl = strfind(website, 'Download');
-    website = website(dl(1):size(website,2));
-    spotURL = strfind(website, '<a href="');
-    website = website((spotURL(1)+9):size(website,2));
-    endofURL = strfind(website,'"');
-    website = website(1:(endofURL-1));
+        if(isempty(strfind(website,'www'))&&isempty(strfind(website,'http')))
+            website = strcat(spotwebsite,website);
+        end
 
-    if(isempty(strfind(website,'www'))&&isempty(strfind(website,'http')))
-        website = strcat(spotwebsite,website);
-    end
-    
-    [path,status] = urlwrite(website,'spot.zip');
+        [path,status] = urlwrite(website,'spot.zip');
 
-    if(status==0)
-        warning('Could not download Spot toolbox from website\n');
+        if(status==0)
+            warning('Warn:noDL','Could not download Spot toolbox from website\n You must have Spot for Sparco to work\n')
+        else
+            fprintf('   Unzipping Spot toolbox...\n');
+            unzip(path,root);
+        end
+
+        delete(path);
+
     else
-        fprintf('   Unzipping Spot toolbox...\n')
-        unzip(path,root)
+        fprintf('Spot toolbox found\n');
     end
     
-    delete(path)
-    
-else
-    fprintf('Spot toolbox found\n')
-end;
-    
-list = dir(root);
+    list = dir(root);
 
-for i=1:length(list)
-    if(~isempty(strfind(list(i).name,'spotbox')))
-        if(list(i).isdir)
-            if(strcmp(list(i).name,'spotbox')) %Rename folder if needed
-                break
-            else
-                spotDirName = list(i).name;
-                newspotDirname = 'spotbox';
-                movefile(spotDirName,newspotDirname);
-                break
+    for i=1:length(list)
+        if(~isempty(strfind(list(i).name,'spotbox')))
+            if(list(i).isdir)
+                if(strcmp(list(i).name,'spotbox')) %Rename folder if needed
+                    break
+                else
+                    spotDirName = list(i).name;
+                    newspotDirname = 'spotbox';
+                    movefile(spotDirName,newspotDirname);
+                    break
+                end
             end
         end
     end
-end
 
-addtopath(root,'spotbox');
+    addtopath(root,'spotbox');
+end
 
 
 % ----------------------------------------------------------------------
